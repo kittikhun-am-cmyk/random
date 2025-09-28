ff5ba190e1797accd6845ad497a20cdb3ecf233a
kittikhun-am-cmyk <kittikhun-am@rmutp.ac.th>
2025-09-28 14:53:44 +0700

    Add files via upload

---
+++ assets/style.php
@@ -0,0 +1,30 @@
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-yJpxAFV0Ip/w63YkZfDWDTU6re/Oc3ZiVqMa97pi8uPt92y0wzeK3UFM2yQRhEom" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="./views/assets/css/superwheel.css">
+<script type="text/javascript" src="./views/assets/js/superwheel.js"></script>
+
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+<style type="text/css">
+	body{
+		overflow-x:hidden;
+		font-family: 'Kanit', sans-serif;
+		background-color:#f3f3f3;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	.swal2-confirm{
+		width:100px;
+		background-color:#28a745;
+	}
+</style>
